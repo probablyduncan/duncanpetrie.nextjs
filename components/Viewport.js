630c0e27e45874f9777822478f5dfe8424565e15
probablyduncan <duncanpetrie1@gmail.com>
2023-07-13 11:26:47 +0100

    test modal

--- components/Viewport.js
+++ components/Viewport.js
@@ -1,4 +1,5 @@
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState, createContext, useRef } from "react";
+import { Paragraph } from "./TextStyles";
 
 // modeled after https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
 
@@ -27,6 +28,42 @@ export function ViewportProvider({ children }) {
         }
     }, []);
 
+    const dialogRef = useRef();
+    useEffect(() => {
+
+        dialogRef.current.showModal();
+
+        const showHideDialog = (on) => {
+            if (on) dialogRef.current.showModal(); 
+            else dialogRef.current.close();
+        }
+
+        const dialogKeyHandler = (e) => {
+            // if (e.code === "ArrowLeft") next(true);
+            // if (e.code === "ArrowRight") next();
+            if (e.code === "Escape") showHideDialog();
+            else if (e.code === "KeyP") showHideDialog(true);
+        }
+        
+        window.addEventListener("keydown", dialogKeyHandler);
+        window.addEventListener('wheel', () => showHideDialog());
+        window.addEventListener('touchmove', () => showHideDialog());
+        
+        return () => {
+            window.removeEventListener("keydown", dialogKeyHandler)
+            window.removeEventListener('wheel', () => showHideDialog());
+            window.removeEventListener('touchmove', () => showHideDialog());
+        };
+
+    }, [])
+
     // return the context provider with mobile bool
-    return <ViewportContext.Provider value={{mobile}}>{children}</ViewportContext.Provider>;
+    return (
+        <ViewportContext.Provider value={{mobile}}>
+            <dialog ref={dialogRef} style={{zIndex: 1000}}>
+                <Paragraph>Heyyy.</Paragraph>
+            </dialog>
+            {children}
+        </ViewportContext.Provider>
+    );
 }
